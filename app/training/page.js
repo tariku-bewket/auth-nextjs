50f28b0a2fb5fbfb11dbae2122b805e7686deca9
Tariku Bewket <tarikubew@gmail.com>
2024-12-25 22:22:39 +0300

    protect routes against unautheneticated access

--- app/training/page.js
+++ app/training/page.js
@@ -1,12 +1,20 @@
+import { redirect } from 'next/navigation';
+
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
 
 export default async function TrainingPage() {
+  const result = await verifyAuth();
+
+  if (!result.user) {
+    return redirect('/');
+  }
   const trainingSessions = getTrainings();
 
   return (
     <main>
       <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
+      <ul id='training-sessions'>
         {trainingSessions.map((training) => (
           <li key={training.id}>
             <img src={`/trainings/${training.image}`} alt={training.title} />
