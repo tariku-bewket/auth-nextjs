3c453475d662c8653d49a0dca057dcd2718ea7d7
Tariku Bewket <tarikubew@gmail.com>
2024-12-26 09:26:10 +0300

    add user logout

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation';
 
 import { hashUserPassword, verifyPassword } from '@/lib/hash';
 import { createUser, getUserByEmail } from '@/lib/user';
-import { createAuthSession } from '@/lib/auth';
+import { createAuthSession, destroyAuth } from '@/lib/auth';
 
 export async function signup(prevState, formData) {
   const email = formData.get('email');
@@ -81,4 +81,9 @@ export async function auth(mode, prevState, formData) {
   }
 
   return await signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroyAuth();
+  redirect('/');
 }
--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,22 +1,21 @@
-import '@/app/globals.css';
+import '../globals.css';
+import { logout } from '@/actions/auth-actions';
 
 export const metadata = {
   title: 'Next Auth',
   description: 'Next.js Authentication',
 };
 
-export default function RootLayout({ children }) {
+export default function AuthRootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body>
-        <header id='auth-header'>
-          <p>Welcome back!</p>
-          <form>
-            <button>Logout</button>
-          </form>
-        </header>
-        {children}
-      </body>
-    </html>
+    <>
+      <header id='auth-header'>
+        <p>Welcome back!</p>
+        <form action={logout}>
+          <button>Logout</button>
+        </form>
+      </header>
+      {children}
+    </>
   );
 }
